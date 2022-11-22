747e0c4ccfe23b888997b9b266f1e7c2341a5924
hacksion <matsui1214@gmail.com>
2022-11-22 16:13:04 +0900

    修正

--- init/db.php
+++ init/db.php
@@ -1,6 +1,6 @@
 <?php
-    define('DATABASE_TYPE', /* データベースタイプ　 => */ 'mysql');
-    define('DATABASE_NAME', /* データベース名 => */ 'cms_admin');
-    define('DATABASE_HOST', /* データベースホスト名　 => */ 'localhost');
-    define('DATABASE_USER', /* データベースユーザー名 => */ 'root');
-    define('DATABASE_PASS', /* データベースパスワード => */ 'root');
+    define('DATABASE_TYPE', /* データベースタイプ　 => */ '');
+    define('DATABASE_NAME', /* データベース名 => */ '');
+    define('DATABASE_HOST', /* データベースホスト名　 => */ '');
+    define('DATABASE_USER', /* データベースユーザー名 => */ '');
+    define('DATABASE_PASS', /* データベースパスワード => */ '');
