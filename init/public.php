2a22f19fd5e2eaa7712123f2041fda55328328ef
M.Toshi <33745694+hacksion@users.noreply.github.com>
2023-02-14 13:34:11 +0900

    リセット

--- init/public.php
+++ init/public.php
@@ -1,9 +1,4 @@
 <?php
 define('PUBLIC_URL', [
-	'URL' => 'https://localhost/FastBlog/',
-	'CURRENT_DIR' => 'FastBlog',
-    'ASYNC' => 'https://localhost/FastBlog/async/',
-    'JS' => 'https://localhost/FastBlog/js/',
-    'CSS' => 'https://localhost/FastBlog/css/',
-    'IMG' => 'https://localhost/FastBlog/images/'
+	'URL' => ''
 ]);
